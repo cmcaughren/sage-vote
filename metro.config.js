5a09706eacc6c4650dd07075b458836be85dbe8c
cmcaughren <cmcaughren@gmail.com>
2025-03-31 19:21:01 -0700

    Updated app.json to identify google services file for ios

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Add support for importing from the app directory
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
+config.resolver.sourceExts.push('mjs');
+config.resolver.sourceExts.push('cjs');
 
 module.exports = config;
